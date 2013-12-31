b2b0c23f2adc46504bc2ed38c479807f8e477de7
Matthew Ewer <Ewer.Matthew@gmail.com>
2013-12-30 21:35:37 -0700

    Made it more specialized for now.  Getting closer.

--- MassContacts/MassContactsViewController.m
+++ MassContacts/MassContactsViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "MassContactsViewController.h"
+#import <AddressBook/AddressBook.h>
 
 @interface MassContactsViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *editSearchFilter;
+@property (nonatomic) ABAddressBookRef book;
+@property (strong, nonatomic) NSArray *unfilteredPeople;
+@property (strong, nonatomic) NSArray *filteredPeople;
+@property (weak, nonatomic) IBOutlet UILabel *labelResults;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @end
 
 @implementation MassContactsViewController
@@ -17,13 +22,97 @@
 //TODO Decide whether to make this more exact.
 #define FILTER_PRESET_EMAILS @".*@.*\\..*"
 
+- (void)setFilteredPeople:(NSArray *)filteredPeople
+{
+    _filteredPeople = filteredPeople;
+    if (filteredPeople) {
+        self.labelResults.text = [NSString stringWithFormat:@"Results: %d of %d", filteredPeople.count, self.unfilteredPeople.count];
+    } else {
+        self.labelResults.text = @"Results: -";
+    }
+    NSLog(@"setFilteredPeople");
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 }
 
-- (IBAction)clickButtonEmail:(id)sender {
-    self.editSearchFilter.text = FILTER_PRESET_EMAILS;
+- (IBAction)clickButtonSearch:(id)sender {
+    //TODO Add error handling.
+    ABAddressBookRef book = ABAddressBookCreateWithOptions(NULL, NULL);
+    [self.activityIndicator startAnimating];
+    __weak MassContactsViewController *weakself = self;
+    ABAddressBookRequestAccessWithCompletion(book, ^(bool granted, CFErrorRef error) {
+        if (granted && !error) {
+            NSLog(@"Success");
+            weakself.book = book;
+            NSArray *people = (__bridge NSArray *)ABAddressBookCopyArrayOfAllPeople(weakself.book);
+            NSArray *filteredPeople = [weakself performSearchOnPeople:people];
+            weakself.unfilteredPeople = people;
+            weakself.filteredPeople = filteredPeople;
+        } else {
+            NSLog(@"Failure");
+            weakself.book = nil;
+        }
+        [weakself.activityIndicator stopAnimating];
+    });
+}
+
+- (NSArray *)performSearchOnPeople:(NSArray *)people
+{
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, NSDictionary *bindings) {
+        ABRecordRef record = (__bridge ABRecordRef)evaluatedObject;
+
+/*
+        NSArray *allPropertiesForABPerson = @[[NSNumber numberWithInt:kABPersonFirstNameProperty],
+                                              [NSNumber numberWithInt:kABPersonLastNameProperty],
+                                              [NSNumber numberWithInt:kABPersonMiddleNameProperty],
+                                              [NSNumber numberWithInt:kABPersonPrefixProperty],
+                                              [NSNumber numberWithInt:kABPersonSuffixProperty],
+                                              [NSNumber numberWithInt:kABPersonNicknameProperty],
+                                              [NSNumber numberWithInt:kABPersonFirstNamePhoneticProperty],
+                                              [NSNumber numberWithInt:kABPersonLastNamePhoneticProperty],
+                                              [NSNumber numberWithInt:kABPersonMiddleNamePhoneticProperty],
+                                              [NSNumber numberWithInt:kABPersonOrganizationProperty],
+                                              [NSNumber numberWithInt:kABPersonJobTitleProperty],
+                                              [NSNumber numberWithInt:kABPersonDepartmentProperty],
+                                              [NSNumber numberWithInt:kABPersonEmailProperty],
+                                              [NSNumber numberWithInt:kABPersonBirthdayProperty],
+                                              [NSNumber numberWithInt:kABPersonNoteProperty],
+                                              [NSNumber numberWithInt:kABPersonCreationDateProperty],
+                                              [NSNumber numberWithInt:kABPersonModificationDateProperty]];
+ */
+
+        NSArray *relevantPropertiesForPerson = @[[NSNumber numberWithInt:kABPersonFirstNameProperty],
+                                                 [NSNumber numberWithInt:kABPersonLastNameProperty],
+                                                 [NSNumber numberWithInt:kABPersonMiddleNameProperty],
+                                                 [NSNumber numberWithInt:kABPersonPrefixProperty],
+                                                 [NSNumber numberWithInt:kABPersonSuffixProperty],
+                                                 [NSNumber numberWithInt:kABPersonNicknameProperty],
+                                                 [NSNumber numberWithInt:kABPersonFirstNamePhoneticProperty],
+                                                 [NSNumber numberWithInt:kABPersonLastNamePhoneticProperty],
+                                                 [NSNumber numberWithInt:kABPersonMiddleNamePhoneticProperty],
+                                                 [NSNumber numberWithInt:kABPersonOrganizationProperty],
+                                                 [NSNumber numberWithInt:kABPersonJobTitleProperty],
+                                                 [NSNumber numberWithInt:kABPersonDepartmentProperty],
+                                                 [NSNumber numberWithInt:kABPersonBirthdayProperty],
+                                                 [NSNumber numberWithInt:kABPersonNoteProperty]];
+        
+        for (NSNumber *property in relevantPropertiesForPerson) {
+            CFTypeRef valueForProperty = ABRecordCopyValue(record, property.intValue);
+            if (valueForProperty) {
+                CFRelease(valueForProperty);
+                return NO;
+            }
+        }
+        
+        return YES;
+    }];
+    return [people filteredArrayUsingPredicate:predicate];
+}
+
+- (IBAction)clickButtonDelete:(id)sender {
 }
 
 @end
